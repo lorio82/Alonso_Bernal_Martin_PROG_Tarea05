667132f0c01ae497847717cfbf4fd36008e05515
malon <malon@DESKTOP-FC0QCP3>
2018-02-04 20:43:52 +0100

    4. Creamos un constructor con cinco parámetros correspondientes a los cinco atributos de la clase y los inicializa si son válidos delo contrario lanza la excepción si el código postal o el DNI no son correctos (comprobados a través de los métodos creados para tal fin).

--- src/tarea05/Cliente.java
+++ src/tarea05/Cliente.java
@@ -1,5 +1,8 @@
 package tarea05;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 
 
 /**
@@ -17,4 +20,46 @@ public class Cliente {
     private int identificador;
     private static int numClientes = 0;  
     
+    public Cliente(Cliente cliente) {
+        nombre = cliente.getNombre();
+        dni = cliente.getDni();
+        direccion = cliente.getDireccion();
+        localidad = cliente.getLocalidad();
+        codigoPostal = cliente.getCodigoPostal();
+        identificador = cliente.getIdentificador();
+    }
+
+    public Cliente(String nombre, String dni, String direccion, String localidad, String codigoPostal) {
+        this.nombre = nombre;
+        
+        if (compruebaDni(dni)) {
+            this.dni = dni;
+        } else {
+            throw new ExcepcionAlquilerVehiculos("El DNI introducido introducido no es válido");
+        }
+        
+        this.direccion = direccion;
+        this.localidad = localidad;
+        
+        if (compruebaCodigoPostal(codigoPostal)) {
+            this.codigoPostal = codigoPostal;
+        } else {
+            throw new ExcepcionAlquilerVehiculos("El Código Postal introducido no válido");
+        }
+        
+        numClientes++;
+        identificador = numClientes;
+    }
+    private boolean compruebaCodigoPostal(String getCodigoPostal) {
+        Pattern pat = Pattern.compile("[0-9]{5}");
+        Matcher mat = pat.matcher(getCodigoPostal);
+        return mat.find();
+    }
+
+    private boolean compruebaDni(String getDni) {
+        Pattern pat = Pattern.compile("[0-9]{8}[A-Z]");
+        Matcher mat = pat.matcher(getDni);
+        boolean compruebaDni = mat.find();
+        return compruebaDni;
+    }
 }
