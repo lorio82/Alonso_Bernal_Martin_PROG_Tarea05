1be9b0ba5b14370bb2077d170d8803763ce8469c
malon <malon@DESKTOP-FC0QCP3>
2018-02-04 20:50:10 +0100

    7. Crea la clase Turismo con sus atributos.

---
+++ src/tarea05/Turismo.java
@@ -0,0 +1,15 @@
+
+package tarea05;
+
+/**
+ *
+ * @author malon
+ */
+
+public class Turismo {
+    private String matricula;
+    private String marca;
+    private String modelo;
+    private int cilindrada;
+    private boolean disponible;
+}
