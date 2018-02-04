99514760d1490ce8200127bfac077a36c5754d44
malon <malon@DESKTOP-FC0QCP3>
2018-02-04 21:16:44 +0100

    17. Creamos la clase AlquilerVehiculos con sus atributos.

---
+++ src/tarea05/AlquilerVehiculos.java
@@ -0,0 +1,16 @@
+
+package tarea05;
+
+/**
+ *
+ * @author malon
+ */
+
+public class AlquilerVehiculos {
+    private final int MAX_CLIENTES = 50;
+    private final int MAX_TURISMOS = 10;
+    private final int MAX_ALQUILERES = 1000;
+    Cliente[] clientes;
+    Turismo[] turismos;
+    Alquiler[] alquileres;
+}
