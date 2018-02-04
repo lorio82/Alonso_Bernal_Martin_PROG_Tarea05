fc3f5fb7f7b2571a8a635af0717b824411735989
malon <malon@DESKTOP-FC0QCP3>
2018-02-04 21:03:42 +0100

    12. Creamos un constructor con dos parámetros que además de inicializar los atributos cliente y turismo, también inicializa la fecha a la fecha actual, dias a cero y pone el turismo como no disponible.

--- src/tarea05/Alquiler.java
+++ src/tarea05/Alquiler.java
@@ -16,4 +16,14 @@ public class Alquiler {
     private Turismo turismo;
     private final SimpleDateFormat FORMATO_FECHA = new SimpleDateFormat("dd/MM/yyyy HH:mm");
     private final int MS_DIA = 1000 * 60 * 60 * 24;
+    
+    public Alquiler(Cliente cliente, Turismo turismo) {
+        this.cliente = cliente;
+        this.turismo = turismo;
+
+        turismo.setDisponible(false);
+
+        fecha = new Date();
+        dias = 0;
+    }
 }
