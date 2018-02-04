e9ec7861cbf096225a6547bbf02a4edf59ac0a98
malon <malon@DESKTOP-FC0QCP3>
2018-02-04 20:54:46 +0100

    8. Creamos un constructor con cuatro parámetros y los inicializa y comprueba que la matrícula y la cilindrada sean correctas.

--- src/tarea05/Turismo.java
+++ src/tarea05/Turismo.java
@@ -1,6 +1,9 @@
 
 package tarea05;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 /**
  *
  * @author malon
@@ -12,4 +15,38 @@ public class Turismo {
     private String modelo;
     private int cilindrada;
     private boolean disponible;
+    
+    public Turismo(Turismo turismo) {
+        matricula = turismo.getMarca();
+        marca = turismo.getMarca();
+        modelo = turismo.getModelo();
+        cilindrada = turismo.getCilindrada();
+        disponible = true;
+    }
+
+    public Turismo(String matricula, String marca, String modelo, int cilindrada) {
+
+        if (compruebaMatricula(matricula)) {
+            this.matricula = matricula;
+        } else {
+            throw new ExcepcionAlquilerVehiculos("La matrícula introducida no es válida");
+        }
+
+        this.marca = marca;
+        this.modelo = modelo;
+
+        if (cilindrada > 0) {
+            this.cilindrada = cilindrada;
+        } else {
+            throw new ExcepcionAlquilerVehiculos("La cilindrada introducida no es válida");
+        }
+
+        disponible = true;
+    }
+    
+    private boolean compruebaMatricula(String matricula) {
+        Pattern pat = Pattern.compile("[0-9]{4}[B-DF-HJ-NP-TV-Z]{3}");
+        Matcher mat = pat.matcher(matricula);
+        return mat.find();
+    }
 }
