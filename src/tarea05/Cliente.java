07a5b5f93f5f04c09cc0729493b290c0a359d501
malon <malon@DESKTOP-FC0QCP3>
2018-02-04 20:36:56 +0100

    3. Creamos la clase Cliente con sus atributos.

---
+++ src/tarea05/Cliente.java
@@ -0,0 +1,20 @@
+package tarea05;
+
+
+
+/**
+ *
+ * @author malon
+ */
+
+public class Cliente {
+    
+    private String nombre;
+    private String dni;
+    private String direccion;
+    private String localidad;
+    private String codigoPostal;
+    private int identificador;
+    private static int numClientes = 0;  
+    
+}
